967cbb2e8f97186199f4ee611f772b3a494e2f00
#云淡然 <cloudcome@163.com>
2016-05-22 16:45:41 +0800

    blear.node.template

---
+++ readme.md
@@ -0,0 +1,15 @@
+# blear.node.template
+
+[![npm module][npm-img]][npm-url]
+[![build status][travis-img]][travis-url]
+[![coverage][coveralls-img]][coveralls-url]
+
+[travis-img]: https://img.shields.io/travis/blearjs/blear.node.template/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/blearjs/blear.node.template
+
+[npm-img]: https://img.shields.io/npm/v/blear.node.template.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/blear.node.template
+
+[coveralls-img]: https://img.shields.io/coveralls/blearjs/blear.node.template/master.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/github/blearjs/blear.node.template?branch=master
+
---
+++ test/test.index.js
@@ -0,0 +1,146 @@
+/**
+ * 测试 文件
+ * @author ydr.me
+ * @create 2016-05-17 12:13
+ */
+
+
+'use strict';
+
+var Template = require('../src/index.js');
+var path = require('path');
+var request = require('supertest');
+var expect = require('chai').expect;
+var express = require('express');
+var howdo = require('blear.utils.howdo');
+
+describe('测试文件', function () {
+    // it('base', function (done) {
+    //     var app = express();
+    //
+    //     app.engine('html', Template.express());
+    //     app.set('views', path.join(__dirname, './base/'));
+    //     app.set('view engine', 'html');
+    //
+    //     app.get('/', function (req, res, next) {
+    //         res.render('index.html', {
+    //             base: true
+    //         });
+    //     });
+    //
+    //     request(app)
+    //         .get('/')
+    //         .expect('content-type', /html/)
+    //         .expect(200)
+    //         .end(function(err, res) {
+    //             if (err) throw err;
+    //
+    //             expect(res.text).to.equal('true');
+    //             done();
+    //         });
+    // });
+
+
+    // it('cache', function (done) {
+    //     var app = express();
+    //
+    //     app.engine('html', Template.express());
+    //     app.set('views', path.join(__dirname, './base/'));
+    //     app.set('view engine', 'html');
+    //     app.set('view cache', true);
+    //
+    //     app.get('/', function (req, res, next) {
+    //         res.render('index.html', {
+    //             base: true
+    //         });
+    //     });
+    //
+    //     app.get('/2', function (req, res, next) {
+    //         res.render('index.html', {
+    //             base: true
+    //         });
+    //     });
+    //
+    //     howdo
+    //         .task(function (next) {
+    //             request(app)
+    //                 .get('/')
+    //                 .expect('content-type', /html/)
+    //                 .expect(200)
+    //                 .end(function (err, res) {
+    //                     if (err) throw err;
+    //
+    //                     expect(res.text).to.equal('true');
+    //                     next();
+    //                 });
+    //         })
+    //         .task(function (next) {
+    //             request(app)
+    //                 .get('/2')
+    //                 .expect('content-type', /html/)
+    //                 .expect(200)
+    //                 .end(function (err, res) {
+    //                     if (err) throw err;
+    //
+    //                     expect(res.text).to.equal('true');
+    //                     next();
+    //                 });
+    //         })
+    //         .follow(done);
+    //
+    //
+    // });
+
+
+    // it('include simple', function (done) {
+    //     var app = express();
+    //
+    //     app.engine('html', Template.express());
+    //     app.set('views', path.join(__dirname, './include/'));
+    //     app.set('view engine', 'html');
+    //
+    //     app.get('/', function (req, res, next) {
+    //         res.render('index.html', {
+    //             base: true
+    //         });
+    //     });
+    //
+    //     request(app)
+    //         .get('/')
+    //         .expect('content-type', /html/)
+    //         .expect(200)
+    //         .end(function (err, res) {
+    //             if (err) throw err;
+    //
+    //             expect(res.text).to.equal('1componenttrue2componenttrue3');
+    //             done();
+    //         });
+    // });
+
+
+    it('include complex', function (done) {
+        var app = express();
+
+        app.engine('html', Template.express());
+        app.set('views', path.join(__dirname, './include/'));
+        app.set('view engine', 'html');
+
+        app.get('/list', function (req, res, next) {
+            res.render('list.html', {
+                list: ['a', 'b']
+            });
+        });
+
+        request(app)
+            .get('/list')
+            .expect('content-type', /html/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) throw err;
+
+                expect(res.text).to.equal('<li>20a</li><li>21b</li>');
+                done();
+            });
+    });
+});
+
